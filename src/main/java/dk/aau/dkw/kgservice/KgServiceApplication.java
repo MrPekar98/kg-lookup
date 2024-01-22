942eafeb900521a5efaf77c45af16ebf403a473c
MrPekar98 <martin.pekar@outlook.dk>
2024-01-22 10:13:59 +0100

    Mutex when constructing indexes

--- src/main/java/dk/aau/dkw/kgservice/KgServiceApplication.java
+++ src/main/java/dk/aau/dkw/kgservice/KgServiceApplication.java
@@ -31,6 +31,7 @@ public class KgServiceApplication implements WebServerFactoryCustomizer<Configur
     private static final String VIRTUOSO_URL = "http://" + System.getenv("VIRTUOSO") + ":8890/sparql";
     private static final String VIRTUOSO_GRAPH_NAME = "http://localhost:8890/" + System.getenv("GRAPH");
     private static Directory dir;
+    private static boolean isLoading = false;
 
     public static void main(String[] args) throws IOException
     {
@@ -47,7 +48,13 @@ public class KgServiceApplication implements WebServerFactoryCustomizer<Configur
     @GetMapping(value = "/index")
     public ResponseEntity<String> index()
     {
+        if (isLoading)
+        {
+            return ResponseEntity.badRequest().body("Indexes are currently being constructed");
+        }
+
         long start = System.currentTimeMillis();
+        isLoading = true;
         System.out.println("Constructing indexes...");
 
         try (VirtuosoIndex graph = new VirtuosoIndex(VIRTUOSO_URL, VIRTUOSO_GRAPH_NAME))
@@ -57,6 +64,7 @@ public class KgServiceApplication implements WebServerFactoryCustomizer<Configur
 
             long duration = System.currentTimeMillis() - start;
             duration = (duration / 1000) / 60;
+            isLoading = false;
             System.out.println("Finished in " + duration + " m");
 
             return ResponseEntity.ok("Indexed KG files in " + duration + "m\n");
@@ -66,6 +74,7 @@ public class KgServiceApplication implements WebServerFactoryCustomizer<Configur
         {
             long duration = System.currentTimeMillis() - start;
             duration = (duration / 1000) / 60;
+            isLoading = false;
             System.err.println("Failed in " + duration + " m: " + e.getMessage());
             e.printStackTrace();
 
